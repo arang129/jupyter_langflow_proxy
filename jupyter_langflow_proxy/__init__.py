c64f96cd65e536d449a6a8be6e87d9ffb64be308
Biju Krishnan <biju.krishnan@gmail.com>
2023-07-21 07:56:53 +0200

    replaced h2o-llm-studio with langflow in init.py

--- jupyter_langflow_proxy/__init__.py
+++ jupyter_langflow_proxy/__init__.py
@@ -1,5 +1,5 @@
 """
-Return config on servers to start for h2o-llm-studio
+Return config on servers to start for langflow
 
 See https://jupyter-server-proxy.readthedocs.io/en/latest/server-process.html
 for more information.
