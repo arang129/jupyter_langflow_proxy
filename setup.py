304e3dd533de5a93c044bf84b0b089ec3b044c7b
arang129 <48246455+arang129@users.noreply.github.com>
2025-02-04 11:22:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="jupyter-langflow-proxy",
-    version="0.2.0",
+    version="0.2.1",
     url="https://github.com/bijucyborg/jupyter_langflow_proxy",
     author="DataSiens",
     description="info@datasiens.ai",
